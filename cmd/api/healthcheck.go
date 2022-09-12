15a40eddbd5ba7c0e7a03c2d6fdeb13cd1056482
kimberly-castillo <2021154911@ub.edu.bz>
2022-09-11 22:15:57 -0600

    healthcheck endpoint returns response in json format

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -3,7 +3,7 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
@@ -13,10 +13,34 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 	// fmt.Fprintln(w, "status: available")
 	// fmt.Fprintf(w, "environment: %s\n", app.config.env)
 	// fmt.Fprintf(w, "version: %s\n", version)
-	js := `{"status": "available", "environment": %q, "version": %q}`
-	js = fmt.Sprintf(js, app.config.env, version)
+
+	//write the json as a http response body
+	//w.Write([]byte(js))
+
+	// one line
+	// js := `{"status": "available", "environment": %q, "version": %q}`
+	// js = fmt.Sprintf(js, app.config.env, version)
+
+	//creating a map to hold the healtcheck data
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	//convert map to json object
+	js, err := json.Marshal(data)
+
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problemand could not process your request", http.StatusInternalServerError)
+		return
+	}
+	//add new line to make viewing on terminal easier
+	js = append(js, '\n')
 	//specifying serve of responses using json
 	w.Header().Set("Content-Type", "application/json")
-	//write the json as a http response body
-	w.Write([]byte(js))
+	//writing out bite slice containing json response body
+	w.Write(js)
+
 }
