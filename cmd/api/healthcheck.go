89efd83f095b60f0a1af865f145c6a31349370cd
kimberly-castillo <2021154911@ub.edu.bz>
2022-09-11 21:37:34 -0600

    working code w new healthgo and temp sayings

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -10,7 +10,13 @@ import (
 // Declare a handler which writes a plain-text response with information about the
 // application status, operating environment and version.
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	// fmt.Fprintln(w, "status: available")
+	// fmt.Fprintf(w, "environment: %s\n", app.config.env)
+	// fmt.Fprintf(w, "version: %s\n", version)
+	js := `{"status": "available", "environment": %q, "version": %q}`
+	js = fmt.Sprintf(js, app.config.env, version)
+	//specifying serve of responses using json
+	w.Header().Set("Content-Type", "application/json")
+	//write the json as a http response body
+	w.Write([]byte(js))
 }
